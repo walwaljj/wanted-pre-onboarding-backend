832d2553dc63585c29dc0533a2293972787fd3f7
Shjung <wjdtngus9@gmail.com>
2023-10-08 18:45:16 +0900

    🐬 docs:  sql 문 추가 - company, user insert

---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+insert into company
+(id, company_name)
+values(1, 'test1'),
+      (2, 'test2'),
+      (3, 'test3');
+
+insert into Users
+(id, username)
+values(1, 'user1'),
+      (2, 'user2'),
+      (3, 'user3');
