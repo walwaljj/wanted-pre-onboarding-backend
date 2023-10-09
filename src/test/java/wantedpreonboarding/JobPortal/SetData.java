27609792f9cd46709b431de844c7996b00be6784
Shjung <wjdtngus9@gmail.com>
2023-10-09 22:01:14 +0900

    :recycle: refactor: user test init 데이터 추가

--- src/test/java/wantedpreonboarding/JobPortal/SetData.java
+++ src/test/java/wantedpreonboarding/JobPortal/SetData.java
@@ -7,23 +7,29 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import wantedpreonboarding.JobPortal.domain.entity.Company;
 import wantedpreonboarding.JobPortal.domain.entity.Job;
+import wantedpreonboarding.JobPortal.domain.entity.User;
+import wantedpreonboarding.JobPortal.repository.CompanyRepository;
 import wantedpreonboarding.JobPortal.repository.JobRepository;
+import wantedpreonboarding.JobPortal.repository.UserRepository;
 
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 
 @SpringBootTest
-@Transactional
 @ActiveProfiles("test")
 public class SetData {
 
     @Autowired
-    protected JobRepository jobRepository;
-    protected Map<Integer, Company> companyMap = new HashMap<>();
+    JobRepository jobRepository;
+
+    @Autowired
+    CompanyRepository companyRepository;
+
+    @Autowired
+    UserRepository userRepository;
 
     public static Integer companySequence = 0;
     public static Integer jobSequence = 0;
+    public static Integer userSequence = 0;
 
     public static Integer getCompanySequence() {
         return ++companySequence;
@@ -32,14 +38,21 @@ public class SetData {
     public static Integer getJobSequence() {
         return ++jobSequence;
     }
+    public static Integer getUserSequence() {
+        return ++userSequence;
+    }
 
     @BeforeEach
     public void init() {
         Company company = new Company(getCompanySequence(), "카카오", new ArrayList<>());
-        companyMap.put(company.getId(), company);
+        companyRepository.save(company);
 
         jobRepository.save(new Job(getJobSequence(), 1, "카카오", "BE", 100000, "BE 포지션", "Java", "KOR", "PUS", new ArrayList<>()));
         jobRepository.save(new Job(getJobSequence(), 1, "카카오", "FE", 200000, "FE 포지션", "JavaScript", "KOR", "PUS", new ArrayList<>()));
         jobRepository.save(new Job(getJobSequence(), 1, "카카오", "BE", 200000, "FE 포지션", "Python", "KOR", "PUS", new ArrayList<>()));
+
+        userRepository.save(new User(getUserSequence(), "test1", new ArrayList<>()));
+        userRepository.save(new User(getUserSequence(), "test2", new ArrayList<>()));
+        userRepository.save(new User(getUserSequence(), "test3", new ArrayList<>()));
     }
 }
